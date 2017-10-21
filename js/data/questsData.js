ad9c68355ac72a3f55e108ab66280a3d8c9a9e31
Alexandr Dragin <one.eastside@gmail.com>
2017-10-21 17:37:18 +0300

    questsData update

--- js/data/questsData.js
+++ js/data/questsData.js
@@ -14,7 +14,230 @@ const questsData = {
     slow: 0,
     total: 0,
     result: ``
-  }
+  },
+
+  questions: [
+    {
+      type: 'two-of-two',
+      question: 'Угадайте для каждого изображения фото или рисунок?',
+      answers: [
+        {
+          image: {
+            url: 'http://placehold.it/468x458',
+            width: 468,
+            height: 458
+          },
+          type: 'photo'
+        },
+        {
+          image: {
+            url: 'http://placehold.it/468x458',
+            width: 468,
+            height: 458
+          },
+          type: 'painting'
+        }
+      ],
+    },
+    {
+      type: 'tinder-like',
+      question: 'Угадай, фото или рисунок?',
+      answers: [
+        {
+          image: {
+            url: 'http://placehold.it/705x455',
+            width: 705,
+            height: 455
+          },
+          type: 'photo'
+        }
+      ]
+    },
+    {
+      type: 'one-of-three',
+      question: 'Найдите рисунок среди изображений',
+      answers: [
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'painting'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        }
+      ]
+    },
+    {
+      type: 'one-of-three',
+      question: 'Найдите рисунок среди изображений',
+      answers: [
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'painting'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        }
+      ]
+    },
+    {
+      type: 'tinder-like',
+      question: 'Угадай, фото или рисунок?',
+      answers: [
+        {
+          image: {
+            url: 'http://placehold.it/705x455',
+            width: 705,
+            height: 455
+          },
+          type: 'photo'
+        }
+      ]
+    },
+    {
+      type: 'two-of-two',
+      question: 'Угадайте для каждого изображения фото или рисунок?',
+      answers: [
+        {
+          image: {
+            url: 'http://placehold.it/468x458',
+            width: 468,
+            height: 458
+          },
+          type: 'photo'
+        },
+        {
+          image: {
+            url: 'http://placehold.it/468x458',
+            width: 468,
+            height: 458
+          },
+          type: 'painting'
+        }
+      ],
+    },
+    {
+      type: 'tinder-like',
+      question: 'Угадай, фото или рисунок?',
+      answers: [
+        {
+          image: {
+            url: 'http://placehold.it/705x455',
+            width: 705,
+            height: 455
+          },
+          type: 'photo'
+        }
+      ]
+    },
+    {
+      type: 'one-of-three',
+      question: 'Найдите рисунок среди изображений',
+      answers: [
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'painting'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        }
+      ]
+    },
+    {
+      type: 'tinder-like',
+      question: 'Угадай, фото или рисунок?',
+      answers: [
+        {
+          image: {
+            url: 'http://placehold.it/705x455',
+            width: 705,
+            height: 455
+          },
+          type: 'photo'
+        }
+      ]
+    },
+    {
+      type: 'one-of-three',
+      question: 'Найдите рисунок среди изображений',
+      answers: [
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'painting'
+        },
+        {
+          'image': {
+            'url': 'http://placehold.it/304x455',
+            'width': 304,
+            'height': 455
+          },
+          'type': 'photo'
+        }
+      ]
+    },
+  ]
 };
 
 export default questsData;

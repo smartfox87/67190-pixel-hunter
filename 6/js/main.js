71d6b42b6325532dde634812f4ef6063a73ec122
Keks <keks@htmlacademy.ru>
2017-10-25 10:00:55 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -32,19 +32,19 @@ const questsData = {
       answers: [
         {
           image: {
-            url: 'http://i.imgur.com/DKR1HtB.jpg',
+            url: `http://i.imgur.com/DKR1HtB.jpg`,
             width: 468,
             height: 458
           },
-          type: 'photo'
+          type: `photo`
         },
         {
           image: {
-            url: 'https://k32.kn3.net/5C7060EC5.jpg',
+            url: `https://k32.kn3.net/5C7060EC5.jpg`,
             width: 468,
             height: 458
           },
-          type: 'painting'
+          type: `painting`
         }
       ],
     },
@@ -54,195 +54,195 @@ const questsData = {
       answers: [
         {
           image: {
-            url: 'http://i.imgur.com/1KegWPz.jpg',
+            url: `http://i.imgur.com/1KegWPz.jpg`,
             width: 705,
             height: 455
           },
-          type: 'photo'
+          type: `photo`
         }
       ]
     },
     {
-      type: 'one-of-three',
-      question: 'Найдите рисунок среди изображений',
+      type: `one-of-three`,
+      question: `Найдите рисунок среди изображений`,
       answers: [
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'painting'
+          'type': `painting`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         }
       ]
     },
     {
-      type: 'one-of-three',
-      question: 'Найдите рисунок среди изображений',
+      type: `one-of-three`,
+      question: `Найдите рисунок среди изображений`,
       answers: [
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'painting'
+          'type': `painting`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         }
       ]
     },
     {
-      type: 'tinder-like',
-      question: 'Угадай, фото или рисунок?',
+      type: `tinder-like`,
+      question: `Угадай, фото или рисунок?`,
       answers: [
         {
           image: {
-            url: 'https://i.imgur.com/DiHM5Zb.jpg',
+            url: `https://i.imgur.com/DiHM5Zb.jpg`,
             width: 705,
             height: 455
           },
-          type: 'photo'
+          type: `photo`
         }
       ]
     },
     {
-      type: 'two-of-two',
-      question: 'Угадайте для каждого изображения фото или рисунок?',
+      type: `two-of-two`,
+      question: `Угадайте для каждого изображения фото или рисунок?`,
       answers: [
         {
           image: {
-            url: 'http://i.imgur.com/1KegWPz.jpg',
+            url: `http://i.imgur.com/1KegWPz.jpg`,
             width: 468,
             height: 458
           },
-          type: 'photo'
+          type: `photo`
         },
         {
           image: {
-            url: 'https://k42.kn3.net/CF42609C8.jpg',
+            url: `https://k42.kn3.net/CF42609C8.jpg`,
             width: 468,
             height: 458
           },
-          type: 'painting'
+          type: `painting`
         }
       ],
     },
     {
-      type: 'tinder-like',
-      question: 'Угадай, фото или рисунок?',
+      type: `tinder-like`,
+      question: `Угадай, фото или рисунок?`,
       answers: [
         {
           image: {
-            url: 'http://placehold.it/705x455',
+            url: `http://placehold.it/705x455`,
             width: 705,
             height: 455
           },
-          type: 'photo'
+          type: `photo`
         }
       ]
     },
     {
-      type: 'one-of-three',
-      question: 'Найдите рисунок среди изображений',
+      type: `one-of-three`,
+      question: `Найдите рисунок среди изображений`,
       answers: [
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'painting'
+          'type': `painting`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         }
       ]
     },
     {
-      type: 'tinder-like',
-      question: 'Угадай, фото или рисунок?',
+      type: `tinder-like`,
+      question: `Угадай, фото или рисунок?`,
       answers: [
         {
           image: {
-            url: 'http://placehold.it/705x455',
+            url: `http://placehold.it/705x455`,
             width: 705,
             height: 455
           },
-          type: 'photo'
+          type: `photo`
         }
       ]
     },
     {
-      type: 'one-of-three',
-      question: 'Найдите рисунок среди изображений',
+      type: `one-of-three`,
+      question: `Найдите рисунок среди изображений`,
       answers: [
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'painting'
+          'type': `painting`
         },
         {
           'image': {
-            'url': 'http://placehold.it/304x455',
+            'url': `http://placehold.it/304x455`,
             'width': 304,
             'height': 455
           },
-          'type': 'photo'
+          'type': `photo`
         }
       ]
     },
